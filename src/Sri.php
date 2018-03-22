ea73910dde02f50b21cc3194b1df4931f1a924b0
Dieter Sting <3165079+Elhebert@users.noreply.github.com>
2018-03-22 18:05:59 +0100

    (fix): clean path for local hashing (#15)
    * (fix): clean path for local hashing
    
    close #11
    
    * (fix): reverse conditionnal
    
    * (fix): use correct parse_url component

--- src/Sri.php
+++ src/Sri.php
@@ -65,7 +65,10 @@ class Sri
         if (starts_with($path, ['http', 'https', '//'])) {
             $fileContent = file_get_contents($path);
         } else {
-            $fileContent = file_get_contents(config('subresource-integrity.base_path')."/{$path}");
+            $path = starts_with($path, '/') ? $path : "/{$path}";
+            $path = parse_url($path, PHP_URL_PATH);
+
+            $fileContent = file_get_contents(config('subresource-integrity.base_path')."{$path}");
         }
 
         if (! $fileContent) {
