1821889c752544055a1718495c69f300ae40fa98
Julio Motol <julio.motol89@gmail.com>
2020-10-25 13:06:14 +0800

    Update config/subresource-integrity.php
    Co-authored-by: Dieter Sting <3165079+Elhebert@users.noreply.github.com>

--- config/subresource-integrity.php
+++ config/subresource-integrity.php
@@ -71,10 +71,9 @@ return [
     | Dangerously Allow Third Party Assets
     |--------------------------------------------------------------------------
     |
-    | This option determines if it should generate a hash for third party
-    | assets. We highly recommend you to set this to false and compute the hash
-    | for this assets manually then add them to the "hashes" config found in
-    | this file.
+    | This option determines if it should generate a hash for the third party assets.
+    | We highly recommend you to set this to false. You should compute the hash
+    | for theses assets manually and add them to the "hashes" config key.
     |
     */
 
